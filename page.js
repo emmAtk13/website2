f78e2a5fcb39eac6cc61f7d9d96a366e427ae78d
Emma <35468679+emmAtk13@users.noreply.github.com>
2019-04-15 22:00:41 -0400

    starts responsive coding

--- page.js
+++ page.js
@@ -1,1 +1,10 @@
 //$('.carousel').carousel()
+
+function navToggle() { // creates a new function named navToggle
+  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
+    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
+    x.className += " responsive";   // then add a new class
+  } else {                          // if it doesn't,
+    x.className = "topnav";         // set class to .topnav
+  } //closes if-else
+} //
