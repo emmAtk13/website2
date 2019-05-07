bc79b0fcbe3d7fa7f1ca66853fd051bc5e28a1bb
Emma <35468679+emmAtk13@users.noreply.github.com>
2019-05-07 14:29:26 -0400

    fixes the img responsiveness

--- page.js
+++ page.js
@@ -1,15 +1,7 @@
 //$('.carousel').carousel()
 
-function navToggle() { // creates a new function named navToggle
-  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
-    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
-    x.className += " responsive";   // then add a new class
-  } else {                          // if it doesn't,
-    x.className = "topnav";         // set class to .topnav
-  } //closes if-else
-} //
 
-
+//$('').scrollspy({ target: '#navbar-example' })
 
 // Get the container element
 //$( '.aside .mysidenav a' ).on( 'click', function () {
